473646c0ec7d82df2e2553cb4d5e1f0e007ace82
carlosfeliz01 <carlosfeliz01@hotmail.com>
2025-03-31 21:22:45 -0400

    Prueba

---
+++ Condiciones_y_Est_Repetitivas.py
@@ -0,0 +1,25 @@
+#CONDICIONES
+
+edad=18
+
+if edad<edad:
+    print("Eres menor de edad. ")
+elif edad  == 18:
+    print("acabo de cumplir 18")
+else:
+    print("eres mayor de edad")
+
+fruta=["Manzana","Pera","Naranja"]
+
+#ESTRUCTURA REPETITIVAS:
+
+#for
+for fruta in fruta:
+    print(fruta)
+
+#while
+contador=0
+while contador <=5:
+    print("Contador: ",contador)
+    contador=contador+1
+
---
+++ Tipo_Datos.py
@@ -0,0 +1,27 @@
+#TIPOS DE DATOS EN PYTON
+
+#string
+saludo="hola mundo"
+print(saludo)
+
+#enteros
+#edad=35
+edad=int(input("Ingrese su edad: "))
+print(edad)
+
+#float
+pi=3.14
+print(pi)
+
+#complex
+ecuacion=1+2j
+print(ecuacion)
+
+#bool
+Es_verdadero=True
+Es_Falso=False
+print(Es_verdadero)
+print(Es_Falso)
+
+#lista
+numero=[1,3.2,"hola"]
